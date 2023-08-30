40dd4ff0627ae27d87c78ec2d418e7db62ca1e1e
Marc Lee <marc@adviso.com.au>
2023-08-30 12:11:29 +1000

    Removes comments

--- solaredge-insights-server/src/lib/find-rate.ts
+++ solaredge-insights-server/src/lib/find-rate.ts
@@ -9,24 +9,11 @@ async function run() {
 
     console.log("date: ", date.toISOString());
 
-    // await ElectricRate.
-    //   findOne({ startDate: { $lte: date }, endDate: { $gte: date } }, 
-    //     'retailer startDate endDate peakCostInCents shoulderCostInCents offPeakCostInCents feedInCostInCents').
-    //   then((rates) => 
-    //     { console.log("result: ", rates ? rates : "nothing" ) });
-
     const rate = await ElectricRate.
       findOne({ startDate: { $lte: date }, endDate: { $gte: date } }, 
         'retailer startDate endDate peakCostInDollars shoulderCostInDollars offPeakCostInDollars feedInCostInDollars');
 
     console.log("rate: ", rate);
-
-    // await ElectricRate.
-    //   findOne({ retailer: "Powershop" }, 
-    //     'retailer startDate endDate peakCostInCents shoulderCostInCents offPeakCostInCents feedInCostInCents').
-    //   then((rate) => 
-    //     { console.log("result: ", rate ? rate : "nothing" ) });        
-
   } catch (err: any) {
     console.log(err.stack);
   } 
